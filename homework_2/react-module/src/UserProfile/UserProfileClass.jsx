5293ddec4696de1b84c04710ba1a47fd59c369fb
valentint123 <valentin.temelkoski388@yahoo.com>
2025-03-25 02:02:00 +0100

    Vtora domasna

--- homework_2/react-module/src/UserProfile/UserProfileClass.jsx
+++ homework_2/react-module/src/UserProfile/UserProfileClass.jsx
@@ -1,18 +1,23 @@
-import './UserProfile.css'; 
-import React from 'react';
-
+import "./UserProfile.css";
+import React from "react";
 
 class UserProfileClass extends React.Component {
   render() {
     const { name, age, hobby } = this.props;
-    
+
     return (
       <div className="user-card">
         <h2>Class-based Component</h2>
         <div className="user-info">
-          <p><strong>Name:</strong> {name}</p>
-          <p><strong>Age:</strong> {age}</p>
-          <p><strong>Hobby:</strong> {hobby}</p>
+          <p>
+            <strong>Name:</strong> {name}
+          </p>
+          <p>
+            <strong>Age:</strong> {age}
+          </p>
+          <p>
+            <strong>Hobby:</strong> {hobby}
+          </p>
         </div>
       </div>
     );
--- homework_2/react-module/src/UserProfile/UserProfileFunction.jsx
+++ homework_2/react-module/src/UserProfile/UserProfileFunction.jsx
@@ -1,15 +1,20 @@
-import './UserProfile.css'; 
-import React from 'react';
-
+import "./UserProfile.css";
+import React from "react";
 
 const UserProfileFunction = ({ name, age, hobby }) => {
   return (
     <div className="user-card">
       <h2>Functional Component</h2>
       <div className="user-info">
-        <p><strong>Name:</strong> {name}</p>
-        <p><strong>Age:</strong> {age}</p>
-        <p><strong>Hobby:</strong> {hobby}</p>
+        <p>
+          <strong>Name:</strong> {name}
+        </p>
+        <p>
+          <strong>Age:</strong> {age}
+        </p>
+        <p>
+          <strong>Hobby:</strong> {hobby}
+        </p>
       </div>
     </div>
   );
